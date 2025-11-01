5826c08cc4a951b0212614aa9c2f6dafc180d609
Ghostxyz <leonelbravo059@gmail.com>
2025-11-01 17:32:02 -0300

    Ejercicio 13

---
+++ Dispositivo.java
@@ -0,0 +1,43 @@
+package ej13;
+
+public class Dispositivo {
+    // << Atributos de Clase >>
+    protected static final double max = 0.01;
+
+    // << Atributos de Instancia >>
+    protected double p1,p2;
+
+    // << Constructor >>
+    public Dispositivo(double pU, double pD){
+        p1 = pU; p2 = pD;
+    }
+
+    // << Comandos >>
+    public void establecerP1(double p){
+        if(p>= 0){
+            p1 = p;
+        }
+    }
+    public void establecerP2(double p){
+        if(p>=0){
+            p2 = p;
+        }
+    }
+    
+    // << Consultas >>
+    public double obtenerP1(){return p1;}
+    public double obtenerP2(){return p2;}
+    public boolean riesgo(){return p2>p1;}
+    public boolean emergencia(){return p1<max;}
+    public boolean equals(Dispositivo d){
+        boolean res = true;
+        if(d == null)res = false;
+        else{
+            res = p1 == d.obtenerP1() && p2 == d.obtenerP2();
+        }
+        return res;
+    }
+    public Dispositivo clone(){
+        return new Dispositivo(p1, p2);
+    }
+}
---
+++ DispositivoFull.java
@@ -0,0 +1,42 @@
+package ej13;
+
+public class DispositivoFull extends Dispositivo {
+    // << Atributos de Instancia >>
+    protected double p3,p4;
+
+    // << Constructor >>
+    public DispositivoFull(double pU, double pD, double pT, double pC){
+        super(pU,pD);
+        p3 = pT; p4 = pC;
+    }
+
+    // << Comandos >>
+    public void establecerP3(double p){
+        if(p>=0){
+            p3 = p;
+        }
+    }
+    public void establecerP4(double p){
+        if(p>=0){
+            p4 = p;
+        }
+    }
+
+    // << Consultas >>
+    public double obtenerP3(){return p3;}
+    public double obtenerP4(){return p4;}
+    public boolean riesgo(){return super.riesgo() || p4>p3;}
+    public boolean emergencia(){return p4<max;}
+    public boolean equals(Dispositivo d){
+        boolean res = true;
+
+        if(d == null){
+            res = false;
+        }else{
+            DispositivoFull df = (DispositivoFull) d;
+            res = super.equals(d) && p3 == df.obtenerP3() && p4 == df.obtenerP4();
+        }
+
+        return res;
+    }
+}
